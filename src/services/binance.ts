7a0142982bdb780b314806df3e754562d7ce95c7
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-08-17 15:19:28 -0700

    use us base url

--- src/services/binance.ts
+++ src/services/binance.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 import type { SymbolType } from "../types.ts";
 
+const BASE_URL = "https://api.binance.us";
+
 export const getCurrentPrice = async (
   symbol: SymbolType
 ): Promise<number | null> => {
   try {
     // Binance free endpoint have rate limits:
     // 1200 requests per minute per ID (public endpoint)
-    const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`;
+    const url = `${BASE_URL}/api/v3/ticker/price?symbol=${symbol}`;
     const response = await axios.get<{ price: string }>(url);
 
     return parseFloat(response.data.price);
