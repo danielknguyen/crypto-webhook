57e6d3f6ec9f47a8bbbc7f9b2cc3758cb36e5570
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-08-17 14:20:21 -0700

    import test route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import "dotenv/config"; // shortcut that tells Node.js to load .env file before anything else runs
 import express from "express";
+import testRoute from "./routes/testRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use("/", testRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
