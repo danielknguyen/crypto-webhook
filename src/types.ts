1f6601b087816ef5400821012becaccf3239af99
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-08-17 14:13:21 -0700

    added enum for Cron Schedule and Colors

--- src/types.ts
+++ src/types.ts
@@ -4,5 +4,20 @@ export enum Symbols {
   SOLUSDT = "SOLUSDT",
 }
 
+export enum CronSchedule {
+  ONE_MINUTE = "*/1 * * * *",
+  FIVE_MINUTES = "*/5 * * * *",
+  FIFTEEN_MINUTES = "*/15 * * * *",
+  THIRTY_MINUTES = "*/30 * * * *",
+  ONE_HOUR = "0 * * * *",
+  FOUR_HOURS = "0 */4 * * *",
+  ONE_DAY = "0 9 * * *", // daily at 9am UTC
+}
+
+export enum Colors {
+  Green = 0x00ff00,
+  Red = 0xff0000,
+}
+
 export type SymbolType = keyof typeof Symbols;
 export type SymbolValue = (typeof Symbols)[SymbolType];
