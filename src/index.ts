f477971b94a868e8f583055706024bb5fb597d3e
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-08-17 15:32:59 -0700

    start scheduler

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import "dotenv/config"; // shortcut that tells Node.js to load .env file before anything else runs
 import express from "express";
+import { startScheduler } from "./services/scheduler.ts";
 import testRoute from "./routes/testRoute.ts";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/", testRoute);
+
+startScheduler();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
